25f728b949cb13b1c3cc108d7418cb36ae52f9b1
Evgeniy Golodnykh <evgenferrum@gmail.com>
2025-05-26 23:50:46 +0300

    update ozon parser and bot buttons

--- src/bot/buttons.py
+++ src/bot/buttons.py
@@ -1,5 +1,7 @@
 from aiogram.types import BotCommand, KeyboardButton
 
+from core.constants import TRACKING_DURATION
+
 START_CMD = BotCommand(command='start', description='Начать работу')
 MENU_CMD = BotCommand(command='menu', description='Главное меню')
 FAVORITE_CMD = BotCommand(command='favorite', description='Избранные товары')
@@ -8,6 +10,7 @@ main_menu_buttons = [
     [KeyboardButton(text='Добавить товар'), KeyboardButton(text='Избранное')]
 ]
 duration_buttons = [
-    [KeyboardButton(text='15 дней'), KeyboardButton(text='30 дней')],
-    [KeyboardButton(text='45 дней'), KeyboardButton(text='60 дней')],
+    [KeyboardButton(text=TRACKING_DURATION[i]),
+     KeyboardButton(text=TRACKING_DURATION[i + 1])]
+    for i in range(0, len(TRACKING_DURATION), 2)
 ]
--- src/parser/ozon.py
+++ src/parser/ozon.py
@@ -2,7 +2,7 @@ import asyncio
 
 from playwright.async_api import async_playwright
 
-stealth_js = """
+stealth_js = '''
 Object.defineProperty(navigator, 'webdriver', { get: () => false });
 
 window.chrome = {
@@ -59,42 +59,46 @@ Object.defineProperty(navigator, 'connection', {
         saveData: false
     })
 });
-"""
+'''
 
 
 async def extract_price(page):
     selectors = ('span.m1q_28.qm1_28.q5m_28', 'div.k7n_28.n7k_28 span')
-
-    for selector in selectors:
-        try:
-            element = page.locator(selector)
-            if await element.count() > 0:
-                text = await element.first.text_content()
-                if text:
-                    price = ''.join(filter(str.isdigit, text))
-                    if price:
-                        return price
-        except Exception:
-            continue
-
-    return 'Цена не найдена'
+    element = page.locator(selectors[0])
+    if await element.count() > 0:
+        text = await element.first.text_content()
+    else:
+        element = page.locator(selectors[1])
+    text = await element.first.text_content()
+    return int(''.join(filter(str.isdigit, text)))
 
 
-async def parse_ozon_product(urls):
-    user_agent = (
-        "Mozilla/5.0 (Windows NT 10.0; Win64; x64) "
-        "AppleWebKit/537.36 (KHTML, like Gecko) "
-        "Chrome/123.0.0.0 Safari/537.36"
-    )
+async def parse_ozon_product(page, url):
+    await page.goto(url, timeout=10000)
+    title = await page.locator('h1').text_content()
+    price = await extract_price(page)
 
-    chrome_args = [
-        "--no-sandbox",
-        "--disable-gpu",
-        "--disable-dev-shm-usage",
-        "--disable-extensions"
-    ]
+    print(f'Название товара: {title.strip()}')
+    print(f'Цена: {price}')
+
+
+async def main(urls):
 
     async with async_playwright() as p:
+
+        user_agent = (
+            'Mozilla/5.0 (Windows NT 10.0; Win64; x64) '
+            'AppleWebKit/537.36 (KHTML, like Gecko) '
+            'Chrome/123.0.0.0 Safari/537.36'
+        )
+
+        chrome_args = (
+            '--no-sandbox',
+            '--disable-gpu',
+            '--disable-dev-shm-usage',
+            '--disable-extensions'
+        )
+
         browser = await p.chromium.launch(
             headless=True,
             args=chrome_args
@@ -102,31 +106,26 @@ async def parse_ozon_product(urls):
 
         context = await browser.new_context(
             user_agent=user_agent,
-            locale="ru-RU",
-            viewport={"width": 1280, "height": 800},
+            locale='ru-RU',
+            viewport={'width': 1280, 'height': 800},
             java_script_enabled=True
         )
 
         await context.add_init_script(stealth_js)
 
-        page = await context.new_page()
-
-        for url in urls:
-            await page.goto(url, timeout=10000)
-
-            title = await page.locator('h1').text_content()
-            price = await extract_price(page)
-
-            print(f'Название товара: {title.strip()}')
-            print(f'Цена: {price}')
+        pages = [await context.new_page() for _ in urls]
+        tasks = [
+            parse_ozon_product(page, url) for page, url in zip(pages, urls)
+        ]
+        await asyncio.gather(*tasks)
 
         await browser.close()
 
 
 if __name__ == '__main__':
-    url1 = 'https://www.ozon.ru/product/kofe-v-zernah-tasty-coffee-braziliya-serrado-1-kg-643726547/'
-    url2 = 'https://www.ozon.ru/product/toughbuilt-tb-h4-30-hb-skladnoy-instrumentalnyy-nozh-s-orlinym-klyuvom-1701173779/'
-    url3 = 'https://www.ozon.ru/product/solvie-proteinovoe-pechene-bez-sahara-assorti-8sht-8-vkusov-35-belka-1841219486/'
+    url1 = 'https://www.ozon.ru/product/pylezashchitnaya-tkan-dlya-vnutrennego-bloka-split-sistemy-konditsionera-5sht-20h80sm-1569927487/'
+    url2 = 'https://www.ozon.ru/product/kamera-dlya-velosipeda-chaoyang-700x33-37c-f-v-48mm-butilovaya-1904782753/'
+    url3 = 'https://www.ozon.ru/product/kreatin-monogidrat-chistyy-bez-vkusa-30-portsiy-creatine-monohydrate-poroshok-200-gr-1376721479/'
     url4 = 'https://www.ozon.ru/product/bobber-chehol-dlya-termosa-flask-770-ml-termochehol-1608374723/'
     url5 = 'https://www.ozon.ru/product/milwaukee-48-22-2903-14-v-1-otvertka-s-hrapovym-mehanizmom-vysokiy-krutyashchiy-moment-1906337729/'
-    asyncio.run(parse_ozon_product((url1, url2, url3, url4, url5, url1, url2, url3, url4, url5, url1, url2, url3, url4, url5, url1, url2, url3, url4, url5)))
+    asyncio.run(main((url1, url2, url3, url4, url5, url1, url2, url3, url4, url5, url1, url2, url3, url4, url5, url1, url2, url3, url4, url5, url1, url2, url3, url4, url5, url1, url2, url3, url4, url5, url1, url2, url3, url4, url5, url1, url2, url3, url4, url5, url1, url2, url3, url4, url5, url1, url2, url3, url4, url5, url1, url2, url3, url4, url5, url1, url2, url3, url4, url5)))
